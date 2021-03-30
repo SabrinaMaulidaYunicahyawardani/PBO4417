d84b4015ecc0f4483e47b07945419389d094bff2
SabrinaMaulidaYunicahyawardani <80633597+SabrinaMaulidaYunicahyawardani@users.noreply.github.com>
2021-03-30 22:29:14 +0700

    Create TestGame.java

---
+++ Pertemuan4/TestGame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sabrina
+ */
+public class TestGame {
+   public static void main(String[]args){
+      GamePlayer pemain = new GamePlayer();
+      pemain.setPosition(60,90);
+      
+      pemain.Run();
+      pemain.Run(30);
+      
+      GameEnemy prajurit = new GameEnemy();
+      prajurit.Run();
+       
+       
+   }
+}
