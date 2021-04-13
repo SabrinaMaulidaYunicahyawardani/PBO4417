dbf92d2ef3f505a6c0f55af2c968ae9456dc09d0
SabrinaMaulidaYunicahyawardani <80633597+SabrinaMaulidaYunicahyawardani@users.noreply.github.com>
2021-04-13 21:36:08 +0700

    Create Bicycle.java

---
+++ Pertemuan6/Praktek/Bicycle.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan6;
+
+/**
+ *
+ * @author Sabrina
+ */
+public class Bicycle {
+    public Bicycle(){
+        
+    }
+    
+    public void ringBell(){
+        System.out.println("kring..kring..");
+    }
+}
